65ebbd4c57f588aa2049a5465d52647dc0a4d1e3
Diyorbek12 <tbest8295@gmail.com>
2022-11-20 16:30:42 +0500

    commit

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -6,10 +6,10 @@ const PostList = ({posts, onDelete, onToggleImportant, onToggleLike}) => {
     const {id, ...itemProps} = item
     return (
       <li key={id} className='list-group-item'>
-          <PostListItem {...itemProps} 
-          onDelete={() => onDelete(id)}
-          onToggleImportant={() => onToggleImportant(id)}
-          onToggleLike={() => onToggleLike(id)} />
+        <PostListItem {...itemProps} 
+        onDelete={() => onDelete(id)}
+        onToggleImportant={() => onToggleImportant(id)}
+        onToggleLike={() => onToggleLike(id)} />
       </li>
     )
   })
